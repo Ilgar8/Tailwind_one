fd62f11c591f8deb2acbedd53e8e28307d403512
unknown <ilgar_guliyev2003@mail.ru>
2023-05-16 14:16:27 +0400

    fixed

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        default: "2rem",
+        sm: "1rem",
+        lg: "1rem",
+        xl: "8rem",
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
